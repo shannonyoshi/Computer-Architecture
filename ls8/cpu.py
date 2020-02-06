31c86de06764f57939b0143d4a14e5de1bbe2ae1
Shannon <shannon.yoshikawa@gmail.com>
2020-02-06 09:14:15 -0800

    completed sprint challenge

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,6 +12,7 @@ class CPU:
         self.register[7] = 0xF4 # stack pointer
         self.ram = [0] * 256  # add 256 bytes
         self.pc = 0 
+        self.flag = 0b00000000
 
     def clean_bin(self, instruction):
         instruction.strip() # removes whitespace
@@ -42,6 +43,15 @@ class CPU:
         # elif op == "SUB": etc
         elif op == "MULT":
             self.register[reg_a] *= self.register[reg_b]
+        elif op == "CMP":
+            # print("\nCMP", self.register[reg_a], "to", self.register[reg_b])
+            if self.register[reg_a] < self.register[reg_b]:
+                self.flag = 0b00000100
+            elif self.register[reg_a] > self.register[reg_b]:
+                self.flag = 0b0000010
+            elif self.register[reg_a] == self.register[reg_b]:
+                self.flag = 0b00000001
+            # print("CMP flag:", self.flag)
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -78,11 +88,11 @@ class CPU:
         value = self.ram[self.pc+2]
         # print(f"Value: {value}")
         self.register[reg_num] = value
-        print(f"LDI: {value} saved to register {reg_num}")
-        print(f"REGISTERS: {self.register}")
+        # print(f"\nLDI: {value} saved to register {reg_num}")
+        # print(f"REGISTERS: {self.register}, self.pc = {self.pc}")
 
     def PRN(self):
-        print("PRINT")
+        # print("\nPRINT")
         reg_num = self.ram[self.pc+1]
         print(self.register[reg_num])
 
@@ -91,7 +101,7 @@ class CPU:
         stack_value = self.ram_read(self.register[7])
         reg_num = self.ram_read(self.pc+1)
         self.register[reg_num] = stack_value
-        print(f"POP {stack_value} to register {reg_num}")
+        # print(f"POP {stack_value} to register {reg_num}")
         self.register[7]+=1
 
     def PUSH(self):
@@ -100,7 +110,7 @@ class CPU:
         reg_num = self.ram_read(self.pc+1) # get the register number
         register_value = self.register[reg_num] #value to be copied
         self.ram_write(self.register[7], register_value)
-        print(f"PUSH value: {self.ram[self.register[7]]} to self.ram {self.register[7]}")
+        # print(f"PUSH value: {self.ram[self.register[7]]} to self.ram {self.register[7]}")
 
     def CALL(self):
         reg_num = self.ram_read(self.pc+1)
@@ -112,9 +122,35 @@ class CPU:
 
     def RET(self):
         stack_value = self.ram_read(self.register[7])
-        print("STACK VALUE", stack_value)
+        # print("STACK VALUE", stack_value)
         self.pc = stack_value
         self.register[7]+=1
+
+    def JMP(self):
+        # print("\nJMP")
+        reg_num = self.ram_read(self.pc+1)
+        reg_value = self.register[reg_num]
+        self.pc = reg_value
+
+    def JEQ(self):
+        # print("\nJEQ")
+        if self.flag == 0b00000001:
+            # print("CMP is equal, flag: 0b00000001")
+            reg_num = self.ram_read(self.pc+1)
+            register_value = self.register[reg_num]
+            self.pc = register_value
+        else:
+            self.pc +=2
+
+    def JNE(self):
+        if self.flag !=1:
+            # print("CMP is NOT equal, flag: ", self.flag)
+            reg_num = self.ram_read(self.pc+1)
+            register_value = self.register[reg_num]
+            self.pc = register_value
+            # print("PC", self.pc)
+        else:
+            self.pc +=2
 
     def func_switcher(self, instruction):
         switcher = {
@@ -122,19 +158,26 @@ class CPU:
             0b01000111: lambda: self.PRN(),
             0b10100010: lambda: self.alu("MULT", self.ram[self.pc+1], self.ram[self.pc+2]),
             0b10100000: lambda: self.alu("ADD", self.ram[self.pc+1], self.ram[self.pc+2]),
+            0b10100111: lambda: self.alu("CMP", self.ram[self.pc+1], self.ram[self.pc+2]),
             0b01000110: lambda: self.POP(),
             0b01000101: lambda: self.PUSH(),
             0b01010000: lambda: self.CALL(),
-            0b00010001: lambda: self.RET()
+            0b00010001: lambda: self.RET(),
+            0b01010101: lambda: self.JEQ(),
+            0b01010100: lambda: self.JMP(),
+            0b01010110: lambda: self.JNE()
         }
         return switcher.get(instruction, lambda: "Invalid command")
 
     def run(self):
         # print("RUN")
         halted = False
-        print(f"Stack pointer = {self.register[7]}")
+        command = 0
+        # print(f"Stack pointer = {self.register[7]}")
         while not halted:
-            print("PC", self.pc)
+            # print(f"command  {command}")
+            command +=1
+            # print("PC", self.pc)
             instruction = self.ram[self.pc]
             # print("instruction in run: ", instruction)
             # print("self.pc: ", self.pc)
@@ -144,7 +187,7 @@ class CPU:
             else:
                 func = self.func_switcher(instruction)
                 func()
-                if instruction not in (0b01010000, 0b00010001):
+                if instruction not in (0b01010000, 0b00010001, 0b01010101, 0b01010100, 0b01010110):
                     operand_count = instruction >> 6
                     instruction_length = operand_count+1
                     self.pc += instruction_length
